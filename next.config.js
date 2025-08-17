8d3e538a17c8fb7de7eec5e9aae1510f8542dab1
prabhat-lab <prabhat.x1@proton.me>
2025-08-17 22:39:38 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  distDir: 'out'
+}
+
+module.exports = nextConfig
